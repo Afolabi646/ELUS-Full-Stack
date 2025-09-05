b2a9be33ac931f022232c354c457a973324c96c6
Afolabi <devafolabi646@gmail.com>
2025-09-05 11:28:11 +0100

    SubCategoryPage.jsx

--- Client/src/pages/SubCategoryPage.jsx
+++ Client/src/pages/SubCategoryPage.jsx
@@ -1,173 +1,173 @@
-// import React, { useEffect, useState } from 'react'
-// import UploadSubCategoryModel from '../components/UploadSubCategoryModel';
-// import AxiosToastError from '../utils/AxiosToastError';
-// import Axios from '../utils/Axios';
-// import SummaryApi from '../common/SummaryApi';
-// import DisplayTable from '../components/DisplayTable';
-// import {  createColumnHelper  } from '@tanstack/react-table'
-// import ViewImage from '../components/ViewImage';
-// import {  LuPencil  } from 'react-icons/lu'
-// import { MdDelete } from "react-icons/md";
-// import EditSubCategory from '../components/EditSubCategory';
-// import ConfirmBox from '../components/ConfirmBox';
+import React, { useEffect, useState } from 'react'
+import UploadSubCategoryModel from '../components/UploadSubCategoryModel';
+import AxiosToastError from '../utils/AxiosToastError';
+import Axios from '../utils/Axios';
+import SummaryApi from '../common/SummaryApi';
+import DisplayTable from '../components/DisplayTable';
+import {  createColumnHelper  } from '@tanstack/react-table'
+import ViewImage from '../components/ViewImage';
+import {  LuPencil  } from 'react-icons/lu'
+import { MdDelete } from "react-icons/md";
+import EditSubCategory from '../components/EditSubCategory';
+import ConfirmBox from '../components/ConfirmBox';
 
-// const SubCategoryPage = () => {
-//   const [openAddSubCategory, setOpenAddSubCategory] = useState(false)
-//   const [data, setData] = useState([])
-//   const [loading, setLoading] = useState(false)
-//   const columnHelper = createColumnHelper()
-//   const [ImageURL, setImageURL] = useState("")
-//   const [openEdit, setOpenEdit] = useState(false)
-//   const [editData, setEditData] = useState({
-//     _id : ""
-//   })
+const SubCategoryPage = () => {
+  const [openAddSubCategory, setOpenAddSubCategory] = useState(false)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const columnHelper = createColumnHelper()
+  const [ImageURL, setImageURL] = useState("")
+  const [openEdit, setOpenEdit] = useState(false)
+  const [editData, setEditData] = useState({
+    _id : ""
+  })
 
-//   const [deleteSubCategory, setDeleteSubCategory] = useState({
-//     _id : ""
-//   })
-//   const [openDeleteConfirmBox, setOpenDeleteConfirmBox] = useState(false)
+  const [deleteSubCategory, setDeleteSubCategory] = useState({
+    _id : ""
+  })
+  const [openDeleteConfirmBox, setOpenDeleteConfirmBox] = useState(false)
 
-//   const fetchSubCategory = async ()=>{
-//     try {
-//       setLoading(true)
-//         const response = await Axios({
-//           ...SummaryApi.getCategory
-//         })
-//         const {  data : responseData  } = response
+  const fetchSubCategory = async ()=>{
+    try {
+      setLoading(true)
+        const response = await Axios({
+          ...SummaryApi.getCategory
+        })
+        const {  data : responseData  } = response
 
-//         if(responseData.success){
-//             setData(responseData.data)
-//         }
-//     } catch (error) {
-//       AxiosToastError(error)
-//     } finally{
-//       setLoading(false)
-//     }
-//   }
+        if(responseData.success){
+            setData(responseData.data)
+        }
+    } catch (error) {
+      AxiosToastError(error)
+    } finally{
+      setLoading(false)
+    }
+  }
 
-//   useEffect(()=>{
-//     fetchSubCategory()
-//   }, [])
+  useEffect(()=>{
+    fetchSubCategory()
+  }, [])
 
-//   const column = [
-//     columnHelper.accessor("name", {
-//       header: "Name",
-//     }),
-//     columnHelper.accessor("image", {
-//       header: "Image",
-//       cell : ({row})=>{
-//         return (
-//           <div className='flex justify-center items-center'>
-//             <img
-//               src={row.original.image}
-//               alt={row.original.name}
-//               className="w-8 h-8 cursor-pointer"
-//               onClick={()=>{
-//                 setImageURL(row.original.image)
-//               }}
-//             />
-//           </div>
-//         );
-//       }
-//     }),
-//     columnHelper.accessor("category", {
-//       header : "Category",
-//       cell : ({row})=>{
-//         return (
-//           <>
-//             {row.original.category &&
-//               row.original.category.map((c, index) => {
-//                 return <p key={index}>{c.name || c}</p>;
-//               })}
-//           </>
-//         );
-//       }
-//     }),
-//    columnHelper.accessor("_id",{
-//     header : "Action",
-//     cell : ({row})=>{
-//       return (
-//         <div className="flex items-center justify-center gap-3">
-//           <button onClick={()=>{
-//             setOpenEdit(true)
-//             setEditData(row.original)
-//             }} className="p-2 bg-green-100 rounded-full hover:text-green-600">
-//             <LuPencil size={20} />
-//           </button>
-//           <button onClick={()=>{
-//             setOpenDeleteConfirmBox(true)
-//             setDeleteSubCategory(row.original)
-//           }} className="p-2 bg-red-100 rounded-full text-red-500 hover:text-red-600">
-//             <MdDelete size={20} />
-//           </button>
-//         </div>
-//       );
-//     }
-//    })
-//   ];
+  const column = [
+    columnHelper.accessor("name", {
+      header: "Name",
+    }),
+    columnHelper.accessor("image", {
+      header: "Image",
+      cell : ({row})=>{
+        return (
+          <div className='flex justify-center items-center'>
+            <img
+              src={row.original.image}
+              alt={row.original.name}
+              className="w-8 h-8 cursor-pointer"
+              onClick={()=>{
+                setImageURL(row.original.image)
+              }}
+            />
+          </div>
+        );
+      }
+    }),
+    columnHelper.accessor("category", {
+      header : "Category",
+      cell : ({row})=>{
+        return (
+          <>
+            {row.original.category &&
+              row.original.category.map((c, index) => {
+                return <p key={index}>{c.name || c}</p>;
+              })}
+          </>
+        );
+      }
+    }),
+   columnHelper.accessor("_id",{
+    header : "Action",
+    cell : ({row})=>{
+      return (
+        <div className="flex items-center justify-center gap-3">
+          <button onClick={()=>{
+            setOpenEdit(true)
+            setEditData(row.original)
+            }} className="p-2 bg-green-100 rounded-full hover:text-green-600">
+            <LuPencil size={20} />
+          </button>
+          <button onClick={()=>{
+            setOpenDeleteConfirmBox(true)
+            setDeleteSubCategory(row.original)
+          }} className="p-2 bg-red-100 rounded-full text-red-500 hover:text-red-600">
+            <MdDelete size={20} />
+          </button>
+        </div>
+      );
+    }
+   })
+  ];
 
-//   const handleDeleteSubCategory = async ()=>{
-//     try {
-//       const response = await Axios({
-//           ...SummaryApi.deleteSubCategory,
-//           data : deleteSubCategory
-//       })
+  const handleDeleteSubCategory = async ()=>{
+    try {
+      const response = await Axios({
+          ...SummaryApi.deleteSubCategory,
+          data : deleteSubCategory
+      })
 
-//       const { data : responseData } = response
+      const { data : responseData } = response
 
-//       if(responseData.success){
-//         toast.success(responseData.message)
-//         fetchSubCategory()
-//         setOpenDeleteConfirmBox(false)
-//         setDeleteSubCategory({_id : ""})
-//       }
-//     } catch (error) {
-//       AxiosToastError(error)
-//     }
-//   }
+      if(responseData.success){
+        toast.success(responseData.message)
+        fetchSubCategory()
+        setOpenDeleteConfirmBox(false)
+        setDeleteSubCategory({_id : ""})
+      }
+    } catch (error) {
+      AxiosToastError(error)
+    }
+  }
 
-//   return (
-//     <section>
-//       <div className="p-2 bg-white shadow-md flex items-center justify-between">
-//         <h2 className="font-semibold">Sub Category</h2>
-//         <button
-//           onClick={() => setOpenAddSubCategory(true)}
-//           className="text-sm border border-yellow-400 hover:bg-yellow-400 px-3 py-1 rounded"
-//         >
-//           Add Sub Category
-//         </button>
-//       </div>
+  return (
+    <section>
+      <div className="p-2 bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Sub Category</h2>
+        <button
+          onClick={() => setOpenAddSubCategory(true)}
+          className="text-sm border border-yellow-400 hover:bg-yellow-400 px-3 py-1 rounded"
+        >
+          Add Sub Category
+        </button>
+      </div>
 
-//       <div className='overflow-auto'>
-//         <DisplayTable data={data} column={column} />
-//       </div>
+      <div className='overflow-auto'>
+        <DisplayTable data={data} column={column} />
+      </div>
 
-//       {openAddSubCategory && (
-//         <UploadSubCategoryModel close={() => setOpenAddSubCategory(false)}
-//         fetchData={fetchSubCategory} />
-//       )}
+      {openAddSubCategory && (
+        <UploadSubCategoryModel close={() => setOpenAddSubCategory(false)}
+        fetchData={fetchSubCategory} />
+      )}
 
-//       {ImageURL && <ViewImage url={ImageURL} close={() => setImageURL("")} />}
+      {ImageURL && <ViewImage url={ImageURL} close={() => setImageURL("")} />}
 
-//       {openEdit && (
-//         <EditSubCategory
-//           data={editData}
-//           close={() => setOpenEdit(false)}
-//           fetchData={fetchSubCategory}
-//         />
-//       )}
+      {openEdit && (
+        <EditSubCategory
+          data={editData}
+          close={() => setOpenEdit(false)}
+          fetchData={fetchSubCategory}
+        />
+      )}
 
-//       {
-//       openDeleteConfirmBox && (
-//         <ConfirmBox
-//         cancel={()=>setOpenDeleteConfirmBox(false)}
-//         close={()=>setOpenDeleteConfirmBox(false)}
-//         confirm={handleDeleteSubCategory}
-//         />
-//       )
-//       }
-//     </section>
-//   );
-// }
+      {
+      openDeleteConfirmBox && (
+        <ConfirmBox
+        cancel={()=>setOpenDeleteConfirmBox(false)}
+        close={()=>setOpenDeleteConfirmBox(false)}
+        confirm={handleDeleteSubCategory}
+        />
+      )
+      }
+    </section>
+  );
+}
 
-// export default SubCategoryPage
+export default SubCategoryPage
